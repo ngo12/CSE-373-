a23d86dc29882e8d16ee5492376bfbdc2eb752e4
setsun8 <setsun8@users.noreply.github.com>
2016-11-21 20:56:25 -0800

    Final Implementation.

--- ExploredGraph.java
+++ ExploredGraph.java
@@ -7,19 +7,13 @@ import java.util.LinkedHashSet;
 import java.util.LinkedHashMap;
 import java.util.Set;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.function.Function;
 
 /**
- * TA Questions...
- * 1. What is count for in the searches?
- * 2. Closed set is just read from and inserted into?
- * 3. Queue for open set in BFS, Stack in IDFS?
- * 4. If we expect search to already be done, why ask for vj in retrievePath?
- */
-
-/**
  * @author Ryan Linden and Brandon Ngo .
- * Extra Credit Options Implemented, if any:  (mention them here.)
+ * Extra Credit Options Implemented, if any: A5E1
  *
  * Solution to Assignment 5 in CSE 373, Autumn 2016
  * University of Washington.
@@ -34,35 +28,32 @@ import java.util.function.Function;
 public class ExploredGraph {
 	Set<Vertex> Ve; // collection of explored vertices
 	Set<Edge> Ee;   // collection of explored edges
-	LinkedHashMap<String, Vertex> pred; // predecessors of explored vertices, i.e. <vertex, vertexPred>
+	LinkedHashMap<String, Vertex> pred;
+
 	Set<Operator> pegMoves;
 	int numPegs;
 
 	/**
 	 * Explored graph constructor. Sets up edge/vertex sets and possible moves using Operator class.
 	 * You can specify number of pegs using this constructor
-	 * @param pegs The number of pegs in Tower-Of-Hanoi
-	 */
-	public ExploredGraph(int pegs) {
-		Ve = new LinkedHashSet<Vertex>();
-		Ee = new LinkedHashSet<Edge>();
-		pred = new LinkedHashMap<String, Vertex>();
-		pegMoves = new LinkedHashSet<Operator>();
-		numPegs = pegs;
-		// Construct possible moves
-		for (int i = 0; i < numPegs; i++) {
-			for (int j = 0; j < numPegs; j++) {
-				if (i != j) { pegMoves.add(new Operator(i, j)); }
-			}
-		}
-	}
-
-	/**
-	 * Explored graph constructor. Sets up edge/vertex sets and possible moves using Operator class.
-	 * Uses the default of 3 pegs.
 	 */
 	public ExploredGraph() {
-		this(3);
+		Ve = new LinkedHashSet<Vertex>();
+		Ee = new LinkedHashSet<Edge>();
+		pred = new LinkedHashMap<String, Vertex>(); // predecessors of explored vertices, i.e. <vertex, vertexPred>
+		pegMoves = new LinkedHashSet<Operator>();
+	}
+
+	int getBracketCount(String vString) {
+		// count brackets to find number of pegs
+		int bracketCount = 0;
+		for (char c : vString.toCharArray()) {
+			if (c == '[') {
+				bracketCount++;
+			}
+		}
+		bracketCount--; // remove extra closing bracket
+		return bracketCount;
 	}
 
 	/**
@@ -70,7 +61,16 @@ public class ExploredGraph {
 	 * @param v The starting vertex.
 	 */
 	public void initialize(Vertex v) {
-		// TODO Check for valid # of pegs?
+
+		// count brackets to find number of pegs
+		int numPegs = getBracketCount(v.toString());
+		// Construct possible moves
+		for (int i = 0; i < numPegs; i++) {
+			for (int j = 0; j < numPegs; j++) {
+				if (i != j) { pegMoves.add(new Operator(i, j)); }
+			}
+		}
+
 		Ve.add(v);
 	}
 
@@ -99,74 +99,68 @@ public class ExploredGraph {
 	public void idfs(Vertex vi, Vertex vj) {
 		boolean endSearch = false; // true when we find vj.
 		// Set Count = 0
-		// int count = 0;
+		int count = 0;
+
 		// Let OPEN = [v0]; Let CLOSED = []
 		Stack<Vertex> openVe = new Stack<Vertex>();
-		LinkedHashSet<Vertex> closedVe = new LinkedHashSet<Vertex>();
+		LinkedList<Vertex> closedVe = new LinkedList<Vertex>();
+
+		// Set Pred(v0) = null;
 		// Set Pred(v0) = null;
 		pred.clear();
 		pred.put(vi.toString(), null);
+		Ee.add(new Edge(null, vi));
+
 		// Add Start vertex to open set
 		openVe.push(vi);
 		// While OPEN is not empty:
-		while (!openVe.empty() && !endSearch) {
+		while (!openVe.empty() || !endSearch) {
 			// v = OPEN.removeFirst()
 			Vertex currentVe = openVe.pop();
+
 			// Set Label(v) = count; count += 1
 			// count++;
+			currentVe.count = count;
+			count++;
+
 			// S = successors(v);
 			// For s in S:
 			for (Operator move : pegMoves) {
-				// if s in OPEN or s in CLOSED, continue.
+				// if s in OPEN or s in CLOSED, continue
 				// else insert s into OPEN at the front.
 				Vertex childVe = move.transition(currentVe);
-				if (childVe.hashCode() != currentVe.hashCode()
-						&& !stackContains(openVe, childVe)
-						&& !setContains(closedVe, childVe)) {
-//						&& !closedVe.contains(childVe)
-//						&& !openVe.contains(childVe)) {
+				if ( (!childVe.equals(currentVe))
+					 && (openVe.search(childVe) == -1)
+					 && (!closedVe.contains(childVe))) {
 					openVe.push(childVe);
+
 					// Set Pred(s) = v.
 					pred.put(childVe.toString(), currentVe);
-					// Add to explored graph?
+
+					// Add to explored graph
 					Ve.add(childVe);
 					Ee.add(new Edge(currentVe, childVe));
+
 				}
-				if (childVe.toString() == vj.toString()) {
+				if (childVe.equals(vj)) {
 					endSearch = true;
 				}
 			}
 			// Insert v into CLOSED
 			closedVe.add(currentVe);
+
+			// For debugging: Used to compare path search to expected path search
+			/*
+			System.out.println(currentVe + ":" + currentVe.count);
+			System.out.println("openVe:");
+			for (Vertex v : openVe) {
+				System.out.println(v.toString());
+			}
+			System.out.println();
+			*/
 		}
 	}
 
-	/**
-	 * Check if vertex is in the Set.
-	 * Used since contains does not work for the vertices...
-	 * @param setVe
-	 * @param vi
-	 * @return
-	 */
-	private boolean setContains(Set<Vertex> setVe, Vertex vi) {
-		Iterator<Vertex> it = setVe.iterator();
-		while (it.hasNext()) {
-			if (it.next().toString().equals(vi.toString())) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean stackContains(Stack<Vertex> stackVe, Vertex vi) {
-		Iterator<Vertex> it = stackVe.iterator();
-		while (it.hasNext()) {
-			if (it.next().toString().equals(vi.toString())) {
-				return true;
-			}
-		}
-		return false;
-	}
 
 	/**
 	 * Find path from vi to vj using Breadth-First search.
@@ -174,23 +168,89 @@ public class ExploredGraph {
 	 * @param vi The start vertex.
 	 * @param vj The end vertex.
 	 */
-	public void bfs(Vertex vi, Vertex vj) {} // Implement this. (Breadth-First Search)
+	public void bfs(Vertex vi, Vertex vj) {
+		boolean endSearch = false;
+
+		// Set count = 0
+		int count = 0;
+
+		// use stack for open set
+		Queue<Vertex> openVe = new LinkedList<Vertex>();
+		Queue<Vertex> closedVe = new LinkedList<Vertex>();
+
+		// Set Pred(v0) = null;
+		pred.clear();
+		pred.put(vi.toString(), null);
+		Ee.add(new Edge(null, vi));
+
+		// Add Start vertex to open set
+		openVe.add(vi);
+		System.out.println(vi);
+		Vertex currentVe;
+
+		// While open set not empty:
+		while (!openVe.isEmpty() || !endSearch) {
+			// pop node from open set
+			currentVe = openVe.remove();
+
+			//System.out.println("CurrentVe:" + currentVe);
+			// Set label(v) = count, count += 1
+			currentVe.count = count;
+			count++;
+
+			for (Operator move : pegMoves) {
+				// if s in OPEN or s in CLOSED, continue.
+				// else insert s into OPEN at the front.
+				Vertex childVe = move.transition(currentVe);
+
+				if ( (!childVe.equals(currentVe))
+					 && (!openVe.contains(childVe))
+				     && (!closedVe.contains(childVe))) {
+
+					openVe.add(childVe);
+
+					// Set Pred(s) = v.
+					pred.put(childVe.toString(), currentVe);
+
+					// Add to explored graph
+					Ve.add(childVe);
+					Ee.add(new Edge(currentVe, childVe));
+				}
+				if (childVe.equals(vj)) {
+					endSearch = true;
+				}
+			}
+			// Insert v into CLOSED
+			closedVe.add(currentVe);
+
+			// For debugging: Used to compare path search to expected path search
+			/*
+			System.out.println(currentVe + ":" + currentVe.count);
+			System.out.println("openVe:");
+			for (Vertex v : openVe) {
+				System.out.println(v.toString());
+			}
+			System.out.println();
+			*/
+		}
+	}
 
 	/**
 	 * Retrieve a path to a vertex using the last tried search method.
 	 * If no search method used, before, we must throw an exception.
-	 * @param vi The vertex to retrieve a path to.
+	 * @param vj The vertex to retrieve a path to.
 	 * @return The path to the specified vertex.
 	 * @throws IllegalArgumentException If no search has been performed yet.
 	 */
-	public ArrayList<Vertex> retrievePath(Vertex vi) {
+
+	public ArrayList<Vertex> retrievePath(Vertex vj) {
 
 		Stack<Vertex> revPath = new Stack<Vertex>();
 		ArrayList<Vertex> path = new ArrayList<Vertex>();
 
-		if (pred.containsKey(vi.toString())) {
+		if (pred.containsKey(vj.toString())) {
 			// Build the path in reverse order.
-			Vertex currentPred = vi;
+			Vertex currentPred = vj;
 			while (pred.get(currentPred.toString()) != null) {
 				revPath.push(currentPred);
 				currentPred = pred.get(currentPred.toString());
@@ -217,7 +277,7 @@ public class ExploredGraph {
 	public ArrayList<Vertex> shortestPath(Vertex vi, Vertex vj) {
 		// Breadth-First gives us the shortest path.
 		bfs(vi, vj);
-		ArrayList<Vertex> path = retrievePath(vi);
+		ArrayList<Vertex> path = retrievePath(vj);
 		return path;
 	}
 
@@ -241,10 +301,17 @@ public class ExploredGraph {
 		ArrayList<Stack<Integer>> pegs; // Each vertex will hold a Towers-of-Hanoi state.
 		// There will be 3 pegs in the standard version, but more if you do extra credit option A5E1.
 		// Constructor that takes a string such as "[[4,3,2,1],[],[]]":
+
+		int count; // Used to keep track of distance from the source vertex during a search
+		int nPegs;
+
 		public Vertex(String vString) {
+
+			nPegs = getBracketCount(vString); // count brackets to find numPegs
+
 			String[] parts = vString.split("\\],\\[");
-			pegs = new ArrayList<Stack<Integer>>(numPegs);
-			for (int i=0; i<numPegs;i++) {
+			pegs = new ArrayList<Stack<Integer>>(nPegs);
+			for (int i=0; i<nPegs;i++) {
 				pegs.add(new Stack<Integer>());
 				try {
 					parts[i]=parts[i].replaceAll("\\[","");
@@ -263,16 +330,45 @@ public class ExploredGraph {
 				catch(NumberFormatException nfe) { nfe.printStackTrace(); }
 			}
 		}
+
 		public String toString() {
 			String ans = "[";
-			for (int i=0; i<numPegs; i++) {
+			for (int i=0; i<nPegs; i++) {
 			    ans += pegs.get(i).toString().replace(" ", "");
-				if (i<numPegs-1) { ans += ","; }
+				if (i<nPegs-1) { ans += ","; }
 			}
 			ans += "]";
 			return ans;
 		}
+
+		/**
+		 * Used to compare one Vertex to another
+		 * Implemented also to check contains/search for LinkedList, Stack, etc.
+		 * @param v The Object to be compared with
+		 * @return Whether or not the Vertex is equal to the Object v
+		 */
+		public boolean equals(Object v){
+
+		     // If the object is compared with itself then return true
+	        if (v == this) {
+	            return true;
+	        }
+
+	        /* Check if o is an instance of Complex or not
+	          "null instanceof [type]" also returns false */
+	        if (!(v instanceof Vertex)) {
+	            return false;
+	        }
+
+	        // typecast o to Complex so that we can compare data members
+	        Vertex cast = (Vertex) v;
+
+	        // Compare the data members and return accordingly
+			return (this.toString().equals(cast.toString()));
+		}
 	}
+
+
 
 	/**
 	 * Edges contain two vertices to show that it is the edge between them
@@ -332,11 +428,16 @@ public class ExploredGraph {
 			this.j = j;
 		}
 
-		// Will determine whether or not this
-		// operator is applicable to this vertex.
-		// Only one disk may be moved at a time. Only a disk that is topmost in its stack of disks may be moved.
-		// It must then be moved either to a peg containing no disks or to a peg
-		// where the topmost disk is of a larger diameter than its own
+
+		/**
+		 * Will determine whether or not this
+		 * operator is applicable to this vertex.
+		 * Only one disk may be moved at a time. Only a disk that is topmost in its stack of disks may be moved.
+		 * It must then be moved either to a peg containing no disks or to a peg
+		 * where the topmost disk is of a larger diameter than its own
+		 * @param v The vertex to check the possible moves from
+		 * @return True if the vertex can move
+		 */
 		public boolean precondition(Vertex v) {
 			if(v.pegs.get(i).empty()){ // Checks if peg i is empty
 				return false;
@@ -350,6 +451,11 @@ public class ExploredGraph {
 
 		}
 
+		/**
+		 * Checks if valid move, if it is, return new vertex, otherwise return old vertex
+		 * @param v The vertex to move to
+		 * @return The new vertex that holds the new pegs state
+		 */
 		public Vertex transition(Vertex v) {
 			if(precondition(v)){
 				Vertex newState = new Vertex(v.toString());
@@ -369,54 +475,47 @@ public class ExploredGraph {
 		}
 	}
 
-//	/**
-//	 * Used for testing purposes
-//	 * @param args
-//	 */
-//	public static void main(String[] args) {
-//		ExploredGraph eg = new ExploredGraph(4);
-//		// Test the vertex constructor:
-//		Vertex v0 = eg.new Vertex("[[5,4,3,2,1],[],[],[]]");
-//		Vertex v1 = eg.new Vertex("[[],[],[],[5,4,3,2,1]]");
-//		System.out.println(v0);
-//		// Add your own tests here.
-//		eg.initialize(v0);
-//		eg.idfs(v0, v1);
-//		ArrayList<Vertex> idfsPath = eg.retrievePath(v1);
-//		Iterator<Vertex> it = idfsPath.iterator();
-//		System.out.println(idfsPath.size());
-//		for (Vertex v : idfsPath) {
-//			System.out.println(v.toString());
-//		}
-//		// The autograder code will be used to test your basic functionality later.
-////		eg.Ve.add(v0); // Adds v0 to the Set of Vertexes in the ExploredGraph
-////		eg.Ve.add(v1); // Adds 1 to the Set of Vertexes in the ExploredGraph
-////		System.out.println(eg.nvertices()); // Size should be equal to 2
-//	}
-
 	/**
-	 * Used for testing purposes
+	 * Used for testing the change in the number of pegs/disks
+	 * http://towersofhanoi.info/Animate.aspx used for test confirmation
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ExploredGraph eg = new ExploredGraph();
+
 		// Test the vertex constructor:
-		Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
-		Vertex v1 = eg.new Vertex("[[],[],[4,3,2,1]]");
+
+		// Used for testing shortest path of 5 disks, should = (2^5) - 1 = 31
+		ExploredGraph eg = new ExploredGraph();
+		Vertex v0 = eg.new Vertex("[[5,4,3,2,1],[],[]]");
+		Vertex v1 = eg.new Vertex("[[],[],[5,4,3,2,1]]");
+
+		/*
+		// Used for testing shortest path of 5 disks, should = (2^8) - 1 = 255
+		ExploredGraph eg = new ExploredGraph();
+		Vertex v0 = eg.new Vertex("[[8,7,6,5,4,3,2,1],[],[]]");
+		Vertex v1 = eg.new Vertex("[[],[],[8,7,6,5,4,3,2,1]]");
+		*/
+
+//		// Used for testing shortest path of 5 disks with 4 pegs, should = 17
+//		ExploredGraph eg = new ExploredGraph();
+//		Vertex v0 = eg.new Vertex("[[6,5,4,3,2,1],[],[],[]]");
+//		Vertex v1 = eg.new Vertex("[[],[],[],[6,5,4,3,2,1]]");
+
+
+//		// Used for testing shortest path of 4 disks with 5 pegs, should = 7
+//		ExploredGraph eg = new ExploredGraph();
+//		Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[],[],[]]");
+//		Vertex v1 = eg.new Vertex("[[],[],[],[],[4,3,2,1]]");
+
 		System.out.println(v0);
 		// Add your own tests here.
 		eg.initialize(v0);
-		eg.idfs(v0, v1);
-		ArrayList<Vertex> idfsPath = eg.retrievePath(v1);
-		Iterator<Vertex> it = idfsPath.iterator();
-		System.out.println(idfsPath.size());
-		for (Vertex v : idfsPath) {
+		ArrayList<Vertex> path = eg.shortestPath(v0, v1);
+		Iterator<Vertex> it = path.iterator();
+		for (Vertex v : path) {
 			System.out.println(v.toString());
 		}
-		// The autograder code will be used to test your basic functionality later.
-//		eg.Ve.add(v0); // Adds v0 to the Set of Vertexes in the ExploredGraph
-//		eg.Ve.add(v1); // Adds 1 to the Set of Vertexes in the ExploredGraph
-//		System.out.println(eg.nvertices()); // Size should be equal to 2
+		System.out.println("pathlength = " + (path.size() - 1));
 	}
 
 }
